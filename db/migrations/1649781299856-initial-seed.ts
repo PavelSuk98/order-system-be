54c1f415847df4931e17eecff32e10625f39ef6d
Pavel Suk <pavelsuk98@gmail.com>
2023-04-09 12:11:56 +0200

    save seed

--- db/migrations/1649781299856-initial-seed.ts
+++ db/migrations/1649781299856-initial-seed.ts
@@ -5,15 +5,15 @@ export default class InitialSeed1649781299856 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner) {
     await queryRunner.query(
-      `INSERT INTO "Role" (id, name, isActive) VALUES('2d142769-a5c1-4a37-a151-aefe097e8934', 'Admin', true)`,
+      `INSERT INTO "Role" (id, name, "isActive") VALUES('2d142769-a5c1-4a37-a151-aefe097e8934', 'Admin', true)`,
     );
 
     await queryRunner.query(
-      `INSERT INTO "User" (id, "userName", email, password, "roleId", "createdDate", "createdBy", "updatedDate", "updatedBy") VALUES('beedecfd-64bb-4c1c-9e8c-76dbdba640b5', 'System Admin', 'admin@dobracajka.cz', '$2a$10$k13JqPFi43ST1ILXKZJqqe1wGzTga79Fgps6iZfo1G/ffmGkf8GkC', '2d142769-a5c1-4a37-a151-aefe097e8934', NOW(), 'System', NOW(), 'System')`,
+      `INSERT INTO "User" (id, "userName", email, password, "roleId", "createdDate", "isActive") VALUES('beedecfd-64bb-4c1c-9e8c-76dbdba640b5', 'System Admin', 'admin@dobracajka.cz', '$2a$10$k13JqPFi43ST1ILXKZJqqe1wGzTga79Fgps6iZfo1G/ffmGkf8GkC', '2d142769-a5c1-4a37-a151-aefe097e8934', NOW(), true)`,
     );
 
     await queryRunner.query(
-      `INSERT INTO "User" (id, "userName", email, password, "roleId", "createdDate", "createdBy", "updatedDate", "updatedBy") VALUES('d286f4f1-a38e-465f-81eb-5cddcac2be79', 'Honza', 'honza@dobracajka.cz', '$2a$10$k13JqPFi43ST1ILXKZJqqe1wGzTga79Fgps6iZfo1G/ffmGkf8GkC', '2d142769-a5c1-4a37-a151-aefe097e8934', NOW(), 'System', NOW(), 'System')`,
+      `INSERT INTO "User" (id, "userName", email, password, "roleId", "createdDate", "isActive") VALUES('d286f4f1-a38e-465f-81eb-5cddcac2be79', 'Honza', 'honza@dobracajka.cz', '$2a$10$k13JqPFi43ST1ILXKZJqqe1wGzTga79Fgps6iZfo1G/ffmGkf8GkC', '2d142769-a5c1-4a37-a151-aefe097e8934', NOW(), true)`,
     );
   }
 
