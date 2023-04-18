22c7cda7cbbefba8b852ea33f59673de9dbc50fd
Tomas <kubin.tom@email.cz>
2023-04-18 17:12:23 +0200

    change order return model

--- src/domains/service/order/models/service-order-table-product.dto.ts
+++ src/domains/service/order/models/service-order-table-product.dto.ts
@@ -1,55 +1,107 @@
+// import { ApiProperty } from '@nestjs/swagger';
+// import { OrderTableProduct } from '@prisma/client';
+
 import { ApiProperty } from '@nestjs/swagger';
 import { OrderTableProduct } from '@prisma/client';
-import { ItemDTO } from 'src/domains/shared/models/item.dto';
+import { Exclude } from 'class-transformer';
+import { ProductDTO } from 'src/domains/admin/product/models/product.dto';
+import { TableDTO } from 'src/domains/admin/table/models/table.dto';
 
-export class ServiceOrderTableProductDTO {
+// import { ItemDTO } from 'src/domains/shared/models/item.dto';
+export class ServiceOrderTableProductDTO implements OrderTableProduct {
   @ApiProperty()
   id: string;
 
   @ApiProperty()
-  productPrice: number;
-  @ApiProperty()
   productOrderDate: Date;
   @ApiProperty()
-  productPreparedDate?: Date;
+  productPreparedDate: Date;
+  @ApiProperty()
+  productPrice: number;
+  @ApiProperty()
+  customerAdditionalRequirements: string;
 
   @ApiProperty()
-  product: ItemDTO;
-  @ApiProperty()
-  productCategory: ItemDTO;
+  product: Partial<ProductDTO>;
 
   @ApiProperty()
-  table: ItemDTO;
-  @ApiProperty()
-  tableArea: ItemDTO;
+  table: Partial<TableDTO>;
 
-  @ApiProperty()
-  customerAdditionalRequirements?: string;
+  @Exclude()
+  tableId: string;
+  @Exclude()
+  productId: string;
+  @Exclude()
+  orderId: string;
+  @Exclude()
+  managedByEmployeeId: string;
+  @Exclude()
+  deleted: Date;
 
-  constructor(data: any) {
-    this.id = data.id;
-    this.productPrice = data.productPrice;
-    this.productOrderDate = data.productOrderDate;
-    this.productPreparedDate = data.productPreparedDate;
-    this.customerAdditionalRequirements = data.customerAdditionalRequirements;
+  constructor({
+    product,
+    table,
+    ...data
+  }: Partial<ServiceOrderTableProductDTO>) {
+    Object.assign(this, data);
 
-    if (data.table) {
-      this.table = new ItemDTO(data.table.id, data.table.name);
-
-      if (data.table.tableArea) {
-        this.tableArea = new ItemDTO(data.table.id, data.table.name);
-      }
+    if (product) {
+      this.product = new ProductDTO(product);
     }
 
-    if (data.product) {
-      this.product = new ItemDTO(data.product.id, data.product.name);
-
-      if (data.product.category) {
-        this.productCategory = new ItemDTO(
-          data.product.category.id,
-          data.product.category.name,
-        );
-      }
+    if (table) {
+      this.table = new TableDTO(table);
     }
   }
 }
+// export class ServiceOrderTableProductDTO {
+//   @ApiProperty()
+//   id: string;
+
+//   @ApiProperty()
+//   productPrice: number;
+//   @ApiProperty()
+//   productOrderDate: Date;
+//   @ApiProperty()
+//   productPreparedDate?: Date;
+
+//   @ApiProperty()
+//   product: ItemDTO;
+//   @ApiProperty()
+//   productCategory: ItemDTO;
+
+//   @ApiProperty()
+//   table: ItemDTO;
+//   @ApiProperty()
+//   tableArea: ItemDTO;
+
+//   @ApiProperty()
+//   customerAdditionalRequirements?: string;
+
+//   constructor(data: any) {
+//     this.id = data.id;
+//     this.productPrice = data.productPrice;
+//     this.productOrderDate = data.productOrderDate;
+//     this.productPreparedDate = data.productPreparedDate;
+//     this.customerAdditionalRequirements = data.customerAdditionalRequirements;
+
+//     if (data.table) {
+//       this.table = new ItemDTO(data.table.id, data.table.name);
+
+//       if (data.table.tableArea) {
+//         this.tableArea = new ItemDTO(data.table.id, data.table.name);
+//       }
+//     }
+
+//     if (data.product) {
+//       this.product = new ItemDTO(data.product.id, data.product.name);
+
+//       if (data.product.category) {
+//         this.productCategory = new ItemDTO(
+//           data.product.category.id,
+//           data.product.category.name,
+//         );
+//       }
+//     }
+//   }
+// }
