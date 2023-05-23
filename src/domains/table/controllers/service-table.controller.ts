a4713aabaea2cbba455b2d1ca18c1987e11b8ce6
Tomas <kubin.tom@email.cz>
2023-05-23 19:49:34 +0200

    add service get tables

---
+++ src/domains/table/controllers/service-table.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Put, UseGuards } from '@nestjs/common/decorators';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/domains/identity/decorators/role.decorator';
+import { UserRoleEnum } from 'src/domains/identity/domain/role.enum';
+import { RoleGuard } from 'src/domains/identity/infrastructure/role.guard';
+import { ListItemModel } from 'src/domains/shared/domain/list-item.interface';
+import { CreateTableDTO } from '../models/create-table.dto';
+import { TableDetailDTO } from '../models/table-detail.dto';
+import { TableDTO } from '../models/table.dto';
+import { UpdateTableDTO } from '../models/update-table.dto';
+import { TableFacade } from '../table.facade';
+@UseGuards(RoleGuard)
+@Controller('v1/Service/Table')
+@ApiTags('Service Table')
+export class ServiceTableController {
+  constructor(private readonly tableFacade: TableFacade) {}
+
+  @Get()
+  @Roles(UserRoleEnum.Service)
+  @ApiResponse({ type: ListItemModel<TableDTO> })
+  async findAll(): Promise<ListItemModel<TableDTO>> {
+    return await this.tableFacade.findAllTableDTO();
+  }
+}
--- src/domains/table/table.module.ts
+++ src/domains/table/table.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ServiceTableController } from './controllers/service-table.controller';
 import { TableAreaController } from './controllers/table-area.controller';
 import { AdminTableStateController } from './controllers/table-state.controller';
 import { AdminTableController } from './controllers/table.controller';
@@ -10,6 +11,7 @@ import { TableHttpModule } from './table-http.module';
     AdminTableStateController,
     TableAreaController,
     AdminTableController,
+    ServiceTableController,
   ],
   providers: [],
 })
